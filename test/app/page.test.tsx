9999e10808d0b5f5b98d7059ec07e059424ad134
JamBalaya56562 <jambalaya.pyoncafe@outlook.jp>
2025-07-02 07:32:12 +0900

    test: add more tests

--- test/app/page.test.tsx
+++ test/app/page.test.tsx
@@ -8,17 +8,22 @@ test("Image test", () => {
   const img = document.querySelectorAll("img")
 
   expect(img[0]?.alt).toEqual("Next.js logo")
+  expect(img[0]?.className).toEqual("dark:invert")
   expect(img[0]?.src).toEqual("/next.svg")
 
   expect(img[1]?.alt).toEqual("Vercel logomark")
+  expect(img[1]?.className).toEqual("dark:invert")
   expect(img[1]?.src).toEqual("/vercel.svg")
 
   expect(img[2]?.alt).toEqual("File icon")
+  expect(img[2]?.className).toEqual("")
   expect(img[2]?.src).toEqual("/file.svg")
 
   expect(img[3]?.alt).toEqual("Window icon")
+  expect(img[3]?.className).toEqual("")
   expect(img[3]?.src).toEqual("/window.svg")
 
   expect(img[4]?.alt).toEqual("Globe icon")
+  expect(img[4]?.className).toEqual("")
   expect(img[4]?.src).toEqual("/globe.svg")
 })
