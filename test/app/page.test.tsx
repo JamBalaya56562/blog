123a308805ed6022cae5119a9b2bb089130d1b8f
JamBalaya56562 <jambalaya.pyoncafe@outlook.jp>
2025-07-02 06:26:28 +0900

    test: update a test

--- test/app/page.test.tsx
+++ test/app/page.test.tsx
@@ -8,17 +8,17 @@ test("Image test", () => {
   const img = document.querySelectorAll("img")
 
   expect(img[0]?.alt).toEqual("Next.js logo")
-  expect(img[0]?.src).toContain("/next.svg")
+  expect(img[0]?.src).toEqual("/next.svg")
 
   expect(img[1]?.alt).toEqual("Vercel logomark")
-  expect(img[1]?.src).toContain("/vercel.svg")
+  expect(img[1]?.src).toEqual("/vercel.svg")
 
   expect(img[2]?.alt).toEqual("File icon")
-  expect(img[2]?.src).toContain("/file.svg")
+  expect(img[2]?.src).toEqual("/file.svg")
 
   expect(img[3]?.alt).toEqual("Window icon")
-  expect(img[3]?.src).toContain("/window.svg")
+  expect(img[3]?.src).toEqual("/window.svg")
 
   expect(img[4]?.alt).toEqual("Globe icon")
-  expect(img[4]?.src).toContain("/globe.svg")
+  expect(img[4]?.src).toEqual("/globe.svg")
 })
