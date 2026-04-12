60d22fc8265af1ef92191a4630d4e9f8bfa9e602
Jam Balaya <jambalaya.pyoncafe@outlook.jp>
2026-04-12 19:22:31 +0900

    feat: add more skelton loadings (#992)

--- components/skeletons/index.ts
+++ components/skeletons/index.ts
@@ -1,4 +1,5 @@
 export { ArticleCardSkeleton } from "./article-card-skeleton"
 export { BlogListSkeleton } from "./blog-list-skeleton"
+export { BlogPostSkeleton } from "./blog-post-skeleton"
 export { HomeContentSkeleton } from "./home-skeleton"
 export { Skeleton } from "./skeleton-primitives"
